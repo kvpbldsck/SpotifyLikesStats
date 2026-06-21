bad1c5d914e9246052dd5f8df610813099911d2d
Valerij Zakharenko <konvpalto87@gmail.com>
2026-06-21 14:08:21 +0200

    Small refactoring

---
+++ SpotifyLikesStats/Application.cs
@@ -0,0 +1,15 @@
+﻿using Presenter;
+using Spotify;
+using Stats;
+
+namespace SpotifyLikesStats;
+
+public sealed class Application(ISpotifyService spotifyService, IStatsService statsService, IViewService viewService)
+{
+    public async Task RunAsync()
+    {
+        var tracks = await spotifyService.GetLikedTracksAsync(1);
+        var stats = statsService.GetStats(tracks);
+        viewService.ShowStats(stats);
+    }
+}
